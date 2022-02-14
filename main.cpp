262b238398ed281fc059b54f8f545f1e4e663519
zhaojinxi <super-string@outlook.com>
2022-02-14 14:41:07 +0800

    1

--- main.cpp
+++ main.cpp
@@ -59,49 +59,5 @@ using namespace std;
 //     return 0;
 // }
 
-class Time
-{
-public:
-    Time(int h = 0, int m = 0, int s = 0)
-    {
-        hour = h;
-        minute = m;
-        second = s;
-    }
-    void operator++(int);
-    void showTime()
-    {
-        cout << "当前时间为：" << hour << ":" << minute << ":" << second << endl;
-    }
+#include <Python.h>
 
-private:
-    int hour, minute, second;
-};
-
-void Time::operator++(int)
-{
-    ++second;
-    if (second == 60)
-    {
-        second = 0;
-        ++minute;
-        if (minute == 60)
-        {
-            minute = 0;
-            hour++;
-            if (hour == 24)
-            {
-                hour = 0;
-            }
-        }
-    }
-}
-
-int main(int argc, char const *argv[])
-{
-    Time t(23, 59, 59);
-    t++;
-    t.showTime();
-    system("pause");
-    return 0;
-}
