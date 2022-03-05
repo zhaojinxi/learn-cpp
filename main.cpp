7cad24b5de171447ac1fbca476fb8a0371925c8a
zhaojinxi <super-string@outlook.com>
2022-03-05 21:51:30 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <typeinfo>
-#include <algorithm>
-#include <limits>
-#include <math.h>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// #include <map>
+// #include <set>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <typeinfo>
+// #include <algorithm>
+// #include <limits>
+// #include <math.h>
 
-using namespace std;
+// using namespace std;
 
 // class Solution {
 // public:
@@ -59,5 +59,45 @@ using namespace std;
 //     return 0;
 // }
 
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include "spam.cpp"
 
+int main(int argc, char *argv[])
+{
+    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+    if (program == NULL)
+    {
+        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+        exit(1);
+    }
+
+    /* Add a built-in module, before Py_Initialize */
+    if (PyImport_AppendInittab("spam", PyInit_spam) == -1)
+    {
+        fprintf(stderr, "Error: could not extend in-built modules table\n");
+        exit(1);
+    }
+
+    /* Pass argv[0] to the Python interpreter */
+    Py_SetProgramName(program);
+
+    /* Initialize the Python interpreter.  Required.
+       If this step fails, it will be a fatal error. */
+    Py_Initialize();
+
+    /* Optionally import the module; alternatively,
+       import can be deferred until the embedded script
+       imports it. */
+    PyObject *pmodule = PyImport_ImportModule("spam");
+    if (!pmodule)
+    {
+        PyErr_Print();
+        fprintf(stderr, "Error: could not import module 'spam'\n");
+    }
+
+    // ...
+
+        PyMem_RawFree(program);
+    return 0;
+}
