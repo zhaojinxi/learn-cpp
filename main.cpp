a2ba89d96a2f1edcb03ccd165b667d129377c86e
zhaojinxi <super-string@outlook.com>
2022-12-19 17:52:20 +0800

    1

--- main.cpp
+++ main.cpp
@@ -12,8 +12,33 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    cout<<100;
-    return 0;
+template <class T>
+class Base {
+public:
+    void hello() {std::cout << "hello\n";}
+
+    void hello1() {std::cout << "hello1\n";}
+
+    template <unsigned N>
+    void hello2() {std::cout << "hello2<" << N << ">()\n";}
+
+};
+
+template <class T>
+class Drivered: public Base<T> {
+public:
+
+    // using Base<T>::hello;
+
+    void test()
+    {
+        hello();
+        this->hello1();
+        this->template hello2<100>();
+ }
+};
+
+int main(int argc, char *argv[]) {
+    Drivered<int> d;
+    d.test();
 }
